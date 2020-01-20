22389cd1d5439d5e8908f004f6a105bdb0e01925
Torkil <torkil.solheim@gmail.com>
2020-01-20 22:50:21 +0100

    testing

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    std::cout << "init pop\n";
+    std::cout << "loopy all below :))\n";
+    std::cout << "find species\n";
+    std::cout << "generate criteria\n";
+    std::cout << "commit genocide\n";
+    std::cout << "fill population\n";
+    std::cout << "evaluate\n";
+    std::cout << "selection\n";
+    return 0;
+}
